e963787d4ddd80f41d51b5c7334d3022f2eed496
ApocalypseFrog <apocalypsefrog@gmail.com>
2021-09-08 18:05:34 +0400

    .

---
+++ file.js
@@ -0,0 +1,93 @@
+// const num = Number(prompt("Введите число:", 0));
+
+// if (num > 0) {
+//     alert ("Число положительное!");
+// }
+// else if (num < 0) {
+//     alert ("Число отрицательное!");
+// }
+// else {alert ("Число равно нулю!");
+// }
+
+
+// const userAge = Number(prompt("Введите свой возраст"));
+
+// if (userAge > 120 || userAge < 0) {
+//     alert ("Возраст не коректный")
+// }
+// else if (userAge < 120 || userAge > 0) {
+//     alert ("Возраст коректный")
+// }
+
+
+// const userNumber = Number(prompt("Введите число"));
+// if (userNumber > 0) {
+//     alert(userNumber);
+// }
+// else if (userNumber < 0){
+//     alert(-userNumber)
+// }
+
+
+// const oclock = Number(prompt("Введите час"));
+// const min = Number(prompt ("Введите минуты"));
+// const sec = Number(prompt ("Введите секунды"));
+
+// if (oclock > 24 || oclock < 0) {
+//     alert("Неправильно");
+// }
+// else if (min > 60 || min < 0) {
+//     alert("Неправильно");
+// }
+// else if (sec > 60 || sec < 0) {
+//     alert("Неправильно");
+// }
+
+// else{
+//  alert("Всё верно")
+// };
+
+
+const numberMonts = Number(prompt("Введите номер месяца?"));
+
+switch (numberMonts) {
+    case 1:
+        alert ("qq");
+        break;
+    case 2:
+        alert ("ww");
+        break;
+    case 3:
+        alert ("ee");
+        break;
+    case 4:
+        alert ("rr");
+        break;
+    case 5:
+        alert ("tt");
+        break;
+    case 6:
+        alert ("aa");
+        break;
+    case 7:
+        alert ("ss");
+        break;
+    case 8:
+        alert ("dd");
+        break;
+    case 9:
+        alert ("ff");
+        break;
+    case 10:
+        alert ("gg");
+        break;
+    case 11:
+        alert ("hh");
+        break;
+    case 12:
+        alert ("z");
+        break;
+    default:
+        alert ("Нет таких значений")
+    
+}
---
+++ test.js
@@ -0,0 +1,12 @@
+// const generateButtom = document.querySelector("#generateButtom");
+
+// generateButtom.addEventListener("click", () => {
+//     const randomNumberNode = document.querySelector("#generateButtom");
+
+//     const randomNumber = Math.floor(Math.random(100) * 100);
+
+//     randomNumberNode.innerHTML = randomNumber;
+// })
+
+const wrap = document.getElementById("wrap");
+
